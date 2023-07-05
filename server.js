ee609b7ef4673e3cbd960c5a50a0a028c511147b
saboshi69 <shui2967@gmail.com>
2023-07-05 15:03:09 -0400

    init backend config for assignment 2

--- server.js
+++ server.js
@@ -15,5 +15,5 @@ app.use("/students", studentRoutes);
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT} OK`);
 });
