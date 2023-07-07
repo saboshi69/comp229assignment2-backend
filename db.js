dae703d04b272c31e3b0fda6e94722a1dbca4301
saboshi69 <shui2967@gmail.com>
2023-07-06 23:49:09 -0400

    real ok

--- db.js
+++ db.js
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
-const uri = isProduction ? process.env.MONGODB_URI : process.env.DB_CONNECT;
-
 mongoose
   .connect(process.env.DB_CONNECT, {
     useNewUrlParser: true,
