c713648aa170fb58ec4aecd0ad3cfc95a3d94fb0
saboshi69 <shui2967@gmail.com>
2023-07-06 23:27:57 -0400

    real ok

--- db.js
+++ db.js
@@ -2,15 +2,12 @@ require("dotenv").config();
 
 const mongoose = require("mongoose");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 mongoose
-  .connect(
-    !!process.env.MONGODB_URI
-      ? process.env.MONGODB_URI
-      : process.env.DB_CONNECT,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(isProduction ? process.env.MONGODB_URI : process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log("MongoDB Connected OK"))
   .catch((err) => console.log("fuck " + err));
